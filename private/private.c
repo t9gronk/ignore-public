bb79ef936bf981efeb354ca9f126cb0aa76a6326
rgronkowski <42407809+rgronkowski@users.noreply.github.com>
2025-10-16 15:21:01 +0200

    initial

---
+++ private/private.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello Private!\n");
+    return 0;
+}
---
+++ public/public.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello Public!\n");
+    return 0;
+}
